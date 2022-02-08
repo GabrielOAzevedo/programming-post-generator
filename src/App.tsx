513bdae9cc5626a97b7c16482651280ad2018df2
gabriel.azevedo <gabriel@goazevedo.dev>
2022-02-08 20:06:53 -0300

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import './App.css';
 import { initialText } from './application/code.context';
 import Design from './components/design/Design';
+import Navbar from './components/navbar/Navbar';
 import TextEditor from './components/texteditor/TextEditor';
 
 function App() {
   const [text, setText] = useState(initialText);
   return (
     <div className="App">
+      <Navbar></Navbar>
       <TextEditor text={text} onChange={setText}></TextEditor>
       <Design text={text}></Design>
     </div>
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import "./navbar.css"
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <h2>Programming post generator</h2>
+        </div>
+    )
+}
