73be1620be52aab947bd2a9e36827acbc5e28733
Gabriel <godeazevedo@gmail.com>
2022-11-07 12:44:52 -0300

    reordering and improved organization

---
+++ src/presentation/App.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./App.css";
+import { initialText } from "../application/code.context";
+import Design from "./components/design/Design";
+import Navbar from "./components/navbar/Navbar";
+import TextEditor from "./components/texteditor/TextEditor";
+
+function App() {
+  const [text, setText] = useState(initialText);
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <TextEditor text={text} onChange={setText}></TextEditor>
+      <Design text={text}></Design>
+    </div>
+  );
+}
+
+export default App;
